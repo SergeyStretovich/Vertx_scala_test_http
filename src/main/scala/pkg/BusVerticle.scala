6fb2737c77eb53f946ad61a1dcb84055452c6d77
Voviandr <voviandr@gmail.com>
2018-01-31 14:43:19 +0200

    commit 5

---
+++ src/main/scala/pkg/BusVerticle.scala
@@ -0,0 +1,20 @@
+package pkg
+
+import io.vertx.lang.scala.ScalaVerticle
+import scala.concurrent.Future
+
+object BusVerticle {
+  val testAddress = "testAddress"
+  val testMessage = "msg"
+  val replyMessage = "message to reply `Hello World!`"
+}
+
+class BusVerticle extends ScalaVerticle {
+  override def startFuture(): Future[_] = {
+    vertx
+      .eventBus
+      .consumer[String](BusVerticle.testAddress)
+      .handler(_.reply(BusVerticle.replyMessage))
+      .completionFuture
+  }
+}
--- src/main/scala/pkg/Vertx_scala_test.scala
+++ src/main/scala/pkg/Vertx_scala_test.scala
@@ -1,8 +1,11 @@
 package pkg
 
+import com.hazelcast.core.Message
 import io.vertx.lang.scala.ScalaVerticle
 import io.vertx.scala.core.Vertx
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object Vertx_scala_test {
@@ -10,10 +13,29 @@ object Vertx_scala_test {
   def main(args: Array[String]): Unit = {
 
     val vertx = Vertx.vertx()
+
     val startFuture = vertx.deployVerticleFuture(ScalaVerticle.nameForVerticle[HttpVerticle])
     startFuture.onComplete {
       case Success(stat) => println(s"Successfully deployed verticle $stat")
       case Failure(ex) => println(s"Failed to deploy verticle $ex")
     }
+
+
+    val busFuture = vertx.deployVerticleFuture(ScalaVerticle.nameForVerticle[BusVerticle])
+    busFuture.onComplete {
+      case Success(stat) => println(s"Successfully deployed verticle $stat")
+      case Failure(ex) => println(s"Failed to deploy verticle $ex")
+    }
+
+    val future: Future[io.vertx.scala.core.eventbus.Message[String]] =
+      vertx
+        .eventBus
+        .sendFuture(BusVerticle.testAddress, BusVerticle.testMessage)
+
+    future.onComplete {
+      case Success(stat) => println(s"received message ${stat.body}")
+      case Failure(ex) => println(s"Failed to receive message $ex")
+    }
+
   }
 }
